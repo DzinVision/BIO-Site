11098dcef3d38ef7294a659594d86009c0a1587c
Vid Drobnič <vid.drobnic@icloud.com>
2014-12-22 20:13:48 +0100

    Changed navbar look to great(sort of)

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,5 @@
 if (window.innerWidth < 995) {
-    document.getElementById("jsNavigation").setAttribute("align", "center");
-    document.getElementById("jsNavigation").setAttribute("style", "margin-left: 0.8em;");
-    document.getElementById("jsNavigation").innerHTML += "<br>";
-    document.getElementById("jsNavigation2").setAttribute("style", "");
-} else {
-    document.getElementById("jsNavigation").setAttribute("style", "position: fixed;");
+    document.querySelector("#jsNavigation").setAttribute("data-spy", "");
+    document.querySelector("#jsNavigationContainer").setAttribute("align", "center");
+    
 }
