e3010849042582ee936acd6ef6e4740c451f3be6
Ziga Patacko Koderman <ziga.patacko@gmail.com>
2014-12-22 14:11:17 +0100

    postrihano 2

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,8 @@
 if (window.innerWidth < 995) {
     document.getElementById("jsNavigation").setAttribute("align", "center");
-    document.getElementById("jsNavigation").setAttribute("style", "");
+    document.getElementById("jsNavigation").setAttribute("style", "margin-left: 0.8em;");
     document.getElementById("jsNavigation").innerHTML += "<br>";
+    document.getElementById("jsNavigation2").setAttribute("style", "");
 } else {
     document.getElementById("jsNavigation").setAttribute("style", "position: fixed;");
 }
