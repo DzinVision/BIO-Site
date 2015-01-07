edf33ef6abd0a3e8a92045aba24c0d10421a27c9
Vid Drobnič <vid.drobnic@icloud.com>
2015-01-07 09:24:20 +0100

    Update addon.js

--- js/addon.js
+++ js/addon.js
@@ -5,6 +5,6 @@ $(document).ready(function() {
         $("#content").removeClass("uk-width-8-10");
         $("#content").addClass("uk-width-1-1");
         $("#nav").addClass("uk-text-center");
-        $("#nav").css("margin-bottom", "15px");
+        $("#nav").css("margin-bottom", "-15px");
     }
 });
